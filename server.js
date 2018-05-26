2432bc1ac9f11ba122eb0d59b04ac55037e471ec
Ben Basuni <ben@scaleapi.com>
2018-05-25 20:19:17 -0700

    testing

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const connect     = require('connect');
 const serveStatic = require('serve-static');
+const nuro        = require('nuro-script');
+const test        = require('test');
 
 connect().use(serveStatic(__dirname)).listen(8080, () => console.log('Server running on 8080...'));
+
+connect().use('/test', function(req, res, next) {
+    console.log(req, res);
+    res.send('test');
+});
